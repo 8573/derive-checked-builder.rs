0a76c5b6d690a08af1337cd0a70f9c9a88c04be5
c74d <8573dd@gmail.com>
2017-08-07 01:10:34 +0000

    Update to second version of eddyb's code
    Update the code to the second version that eddyb gave me, which allows
    having both required and optional fields.

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,59 @@
 #[macro_export]
 macro_rules! bld {
-    ({ $($name:ident: $ty:ty),* } $($full_methods:tt)*) => {
+    (@setters $Builder:ident: optional {
+        $($no:ident)*
+    } ;$($rest:tt)*) => {};
+    (@setters $Builder:ident: optional {
+        $($no:ident)*
+    } ($name:ident: $ty:ty) $(($na:ident: $ta:ty))*;$(($nb:ident: $tb:ty))*) => {
         #[allow(non_camel_case_types)]
-        struct Builder<$($name=()),*> {
-            $($name: $name),*
-        }
-
-        impl Default for Builder {
-            fn default() -> Builder {
-                Builder { $($name: (),)* }
-            }
-        }
-
-        bld!(@setters $(($name: $ty))*;);
-
-        impl Builder<$($ty),*> {
-            $($full_methods)*
-        }
-    };
-    (@setters ;$($rest:tt)*) => {};
-    (@setters ($name:ident: $ty:ty) $(($na:ident: $ta:ty))*;$(($nb:ident: $tb:ty))*) => {
-        #[allow(non_camel_case_types)]
-        impl<$($nb,)* $($na,)*> Builder<$($nb,)* (), $($na,)*> {
-            fn $name<$name: Into<$ty>>(self, $name: $name) -> Builder<$($nb,)* $ty, $($na,)*> {
-                Builder {
+        impl<$($nb,)* $($na,)*> $Builder<$($nb,)* (), $($na,)*> {
+            fn $name<$name: Into<$ty>>(self, $name: $name)
+                                       -> $Builder<$($nb,)* $ty, $($na,)*> {
+                $Builder {
                     $($nb: self.$nb,)*
                     $name: $name.into(),
                     $($na: self.$na,)*
+                    $($no: self.$no,)*
                 }
             }
         }
 
-        bld!(@setters $(($na: $ta))*; $(($nb: $tb))* ($name: $ty));
+        bld!(@setters $Builder: optional {
+            $($no)*
+        } $(($na: $ta))*; $(($nb: $tb))* ($name: $ty));
+    };
+    ($Builder:ident: required {
+        $($nr:ident: $tr:ty),*
+    } optional {
+        $($no:ident: $to:ty),*
+    } $($full_methods:tt)*) => {
+        #[allow(non_camel_case_types)]
+        struct $Builder<$($nr=()),*> {
+            $($nr: $nr,)*
+            $($no: Option<$to>,)*
+        }
+
+        impl Default for $Builder {
+            fn default() -> Self {
+                $Builder {
+                    $($nr: (),)*
+                    $($no: None,)*
+                }
+            }
+        }
+
+        bld!(@setters $Builder: optional { $($no)* } $(($nr: $tr))*;);
+
+        #[allow(non_camel_case_types)]
+        impl<$($nr),*> $Builder<$($nr,)*> {
+            $(fn $no<$no: Into<$to>>(self, $no: $no) -> Self {
+                $Builder { $no: Some($no.into()), ..self }
+            })*
+        }
+
+        impl $Builder<$($tr),*> {
+            $($full_methods)*
+        }
     }
 }
--- tests/eddyb-1.rs
+++ tests/eddyb-1.rs
@@ -2,17 +2,20 @@
 extern crate checked_builder;
 
 bld! {
-    {
+    Foo: required {
         a: String,
         b: i32,
         c: [u8; 3]
+    } optional {
+        d: String
     }
     fn print(self) {
-        println!("{} {} {:?}", self.a, self.b, &self.c[..]);
+        println!("{} {} {:?} {:?}", self.a, self.b, &self.c[..], self.d);
     }
 }
 
 #[test]
 fn main() {
-    Builder::default().a("foo").b(5).c([1, 2, 3]).print();
+    Foo::default().a("foo").b(5).c([1, 2, 3]).print();
+    Foo::default().a("bar").b(42).c([0, 0, 0]).d("quux").print();
 }
